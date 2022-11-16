46a0c224eab21b0ea2214e41e8265516fba30739
Merle Makoben <merle.makoben@neuefische.de>
2022-11-16 10:04:59 +0000

    Add solution for challenge

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,33 @@
-export function createCharacterCard() {}
+export default function createCard(character) {
+  const card = document.createElement("li");
+  card.classList.add("card");
+
+  function type() {
+    if (character.type === "") {
+      return character.species;
+    } else {
+      return character.type;
+    }
+  }
+
+  card.innerHTML = `<div class="card__image-container">
+    <img
+      class="card__image"
+      src= ${character.image}
+      alt=${character.name}
+    />
+    <div class="card__image-gradient"></div>
+  </div>
+  <div class="card__content">
+    <h2 class="card__title">${character.name}</h2>
+    <dl class="card__info">
+      <dt class="card__info-title">Status</dt>
+      <dd class="card__info-description">${character.status}</dd>
+      <dt class="card__info-title">Type</dt>
+      <dd class="card__info-description">${type()}</dd>
+      <dt class="card__info-title">Occurrences</dt>
+      <dd class="card__info-description">${character.episode.length}</dd>
+    </dl>
+  </div>`;
+  return card;
+}
